8ea7de2bd1cf4cd2fb533340b3d3425d7ba752c4
Felipe <machadofelipe2016@outlook.com>
2022-06-30 17:26:05 -0300

    c17-adding new routers and adding new imports componets1

--- more-router/src/App.js
+++ more-router/src/App.js
@@ -5,6 +5,8 @@ import Sobre from './pages/home';
 import Nav from './Navbar/Nav'
 import AnotherSobre from './pages/AnotherSobre';
 import Product from './pages/Product';
+import Info from './pages/Info';
+import NotFound from './pages/NotFound';
 
 function App() {
  
@@ -15,7 +17,12 @@ return ( <div className='App' >
     <Routes>
       <Route path='/' element={<Sobre/>} />
       <Route path='/sobre' element={<AnotherSobre/> } />
-      <Route path='/products/:id' element={<Product/>}  />   
+      <Route path='/products/:id' element={<Product/>}  /> 
+      {/* nested router */}
+      <Route path='/products/:id/info' element={ <Info/>}  />
+      
+      {/*last router created about 404*/}
+      <Route path='*' element= {<NotFound/>}  />
     </Routes>
     
   </div> );
