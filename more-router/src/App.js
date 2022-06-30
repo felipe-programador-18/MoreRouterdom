25fb87628925cca4b44750cca8c45122b485e27a
Felipe <machadofelipe2016@outlook.com>
2022-06-30 18:26:58 -0300

    c22-adding search component and another things in principles page

--- more-router/src/App.js
+++ more-router/src/App.js
@@ -7,6 +7,8 @@ import AnotherSobre from './pages/AnotherSobre';
 import Product from './pages/Product';
 import Info from './pages/Info';
 import NotFound from './pages/NotFound';
+import SearchBar from './Navbar/SearchBar';
+import Search from './pages/Search';
 
 function App() {
  
@@ -14,6 +16,7 @@ function App() {
 return ( <div className='App' >
      <h1>React Router</h1>
      <Nav/>
+     <SearchBar/>
     <Routes>
       <Route path='/' element={<Sobre/>} />
       <Route path='/sobre' element={<AnotherSobre/> } />
@@ -21,6 +24,7 @@ return ( <div className='App' >
       {/* nested router */}
       <Route path='/products/:id/info' element={ <Info/>}  />
       
+      <Route path='/search' element={<Search/> }  />
       {/*last router created about 404*/}
       <Route path='*' element= {<NotFound/>}  />
     </Routes>
