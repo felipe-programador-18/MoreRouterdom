a2e552f3601b4f9f27488231970ea072d71fa303
Felipe <machadofelipe2016@outlook.com>
2022-06-30 17:25:03 -0300

    c16-adding navlink inside nav bar

--- more-router/src/Navbar/Nav.js
+++ more-router/src/Navbar/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import '../App.css' ;
 
 const Nav = () => {
@@ -7,8 +7,11 @@ const Nav = () => {
     return(
     <div className='App' >
      <nav>
-        <Link to='/' > Home </Link>
-        <Link to='/sobre' >Sobre</Link>
+        {/*<Link to='/' > Home </Link>
+        <Link to='/sobre' >Sobre</Link>*/}
+       
+      <NavLink to='/' >Home</NavLink>
+      <NavLink to='/sobre' >Sobre</NavLink>
      </nav>
      </div>  )
 }
