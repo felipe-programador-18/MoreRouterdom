72821cdac0f572e63b8aa1fe89b192c6e97db7b1
Felipe <machadofelipe2016@outlook.com>
2022-06-30 15:39:09 -0300

    c7-deleted someone dates

--- more-router/src/App.js
+++ more-router/src/App.js
@@ -6,22 +6,8 @@ import Nav from './Navbar/Nav'
 import AnotherSobre from './pages/AnotherSobre';
 import Product from './pages/Product';
 
-
-//const url = "http://localhost:3000/products"
 function App() {
- // const [products, setProduct] = useState([])
-
-  //useEffect(() => {
-    //const Fetching = async () => {
-    //  const res = await fetch(url)
-      //const data = await res.json()
-      // setProduct(data)
-    //}
  
-    //Fetching()
-  
-  //},[])
-
   
 return ( <div className='App' >
      <h1>React Router</h1>
