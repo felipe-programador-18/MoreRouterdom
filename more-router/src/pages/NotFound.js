ac149a21922fe7bbcffcab2f97260f3e43869e06
Felipe <machadofelipe2016@outlook.com>
2022-06-30 17:21:18 -0300

    c12-create page notFound

---
+++ more-router/src/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (<>
+  <div className='notfound' > 404 Not found 
+    <p className='notfound' > not have nothing here ! </p> </div>
+  </>)
+}
+
+export default NotFound
