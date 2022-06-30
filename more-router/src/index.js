d0b7352bbc2aacc807841f378c348a731c0b5763
Felipe <machadofelipe2016@outlook.com>
2022-06-30 18:25:49 -0300

    c21-rearrange code

--- more-router/src/index.js
+++ more-router/src/index.js
@@ -11,12 +11,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-   
       <App />
-       
-    </BrowserRouter>
-  
-  
+    </BrowserRouter>  
   </React.StrictMode>
 );
 
