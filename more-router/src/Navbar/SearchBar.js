d9423e30528c3c60968930224e6f3b36001af54c
Felipe <machadofelipe2016@outlook.com>
2022-07-01 15:46:38 -0300

    c26- somethigs change1

--- more-router/src/Navbar/SearchBar.js
+++ more-router/src/Navbar/SearchBar.js
@@ -1,23 +1,23 @@
 import { useNavigate } from 'react-router-dom'
-import React,{useState} from 'react'
+import {useState} from 'react'
 
-const SearchBar = () => {
+export const SearchBar = () => {
   const navigate = useNavigate()
   const [query, setQuery] = useState()
 
   const handlSumit = (e) => {
-    e.preventDefault()
-    navigate('/search?q'+ query)
+    e.preventDefault();
+    navigate("/search?q=" + query)
 
   }
 
    return(<>
          <form onSubmit={handlSumit} >
             <label>
-              <input type='text' name='search' onClick={(e)=>  setQuery(e.target.value) }  />
+              <input type='text' onClick={(e)=>  setQuery(e.target.value) }  />
             </label>
             <label>
-                <input type='submit'  value='search' />
+                <input type='submit'  value='Buscar' />
             </label>
 
          </form>
@@ -26,4 +26,3 @@ const SearchBar = () => {
    </>)
 }
 
-export default SearchBar
