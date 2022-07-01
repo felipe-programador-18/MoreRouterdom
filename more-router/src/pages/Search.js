8b4cff06bd43bad954324674fd221da8663c272a
Felipe <machadofelipe2016@outlook.com>
2022-07-01 15:45:38 -0300

    c24-removed useEffect and adding loading and trade name about mp and key

--- more-router/src/pages/Search.js
+++ more-router/src/pages/Search.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import { useSearchParams, Link } from "react-router-dom"
 import { useFetch } from "../hooks/useFetch"
 
@@ -7,15 +8,17 @@ const Search  = () => {
  const url ="http://localhost:3000/products?" + searchParams
 
  const {data:items, loading, error} = useFetch(url)
-    
+
+
     return (<>
     <div> 
     <h1>Search new survey</h1>
+    {loading && <p>Loading search Product....</p>}
            <ul className='products'>
-              { items && items.map((item) => (<li key={item.id}>
-                <h1> {item.name} </h1> 
-                <p>  {item.price} </p>  
-                <Link to={`/products/${item.id}`} >Details!</Link>
+              { items && items.map((product) => (<li key={product.id}>
+                <h1> {product.name} </h1> 
+                <p>  {product.price} </p>  
+                <Link to={`/products/${product.id}`} >Details!</Link>
                 </li>))}
            </ul>
     </div>
