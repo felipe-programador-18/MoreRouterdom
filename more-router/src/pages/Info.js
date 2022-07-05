f5648b95bc8297b70edb2eb408d90893eb1ecb1d
Felipe <machadofelipe2016@outlook.com>
2022-07-05 13:11:25 -0300

    c29- adding link to adding some router to back home

--- more-router/src/pages/Info.js
+++ more-router/src/pages/Info.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams ,Link } from 'react-router-dom'
 
 //remerber i can get data of id together work with useParams/
 const Info = () => {
@@ -10,6 +10,9 @@ return ( <div>
 
         <h1>More information about Products!! {id} </h1>
          <h2>This product is lot good, also have price much affordable for you acquiring!!</h2>
+         <h3>This product is very interesting for you wanna traning or improve your health running!!</h3>
+         {/* adding link to practice and create new idea for evolving how developer !!!  */}
+         <Link to='/' > back home</Link>
     </div> )
 }
 
