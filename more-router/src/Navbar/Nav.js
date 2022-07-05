12ab0dbcb0ef05fdc2a34b29ee809a11b1b69b3a
Felipe <machadofelipe2016@outlook.com>
2022-07-05 13:13:59 -0300

    c31-removed link and code comments

--- more-router/src/Navbar/Nav.js
+++ more-router/src/Navbar/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import {  NavLink } from 'react-router-dom'
 import '../App.css' ;
 
 const Nav = () => {
@@ -7,8 +7,7 @@ const Nav = () => {
     return(
     <div className='App' >
      <nav>
-        {/*<Link to='/' > Home </Link>
-        <Link to='/sobre' >Sobre</Link>*/}
+    
        
       <NavLink to='/' >Home</NavLink>
       <NavLink to='/sobre' >Sobre</NavLink>
